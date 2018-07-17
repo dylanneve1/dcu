b63fbbd9695c5be6e87d8703fa4e201d0adaa108
dylanneve1 <dylanneve1@gmail.com>
2018-07-17 18:46:37 +0100

    Add window test and finish prime number program

--- prime_numbers.py
+++ prime_numbers.py
@@ -1,11 +1,19 @@
-lower = int(input("Enter lower end of range :"))
+lower = int(input("\nEnter lower end of range :"))
 upper = int(input("Enter upper end of range :"))
 primes = []
 
-print("\nChecking for prime numbers between " + str(lower) + " and " + str(upper) + "\n\n")
+print("\nChecking for prime numbers between",lower,"and",upper, end = "\n\n")
 
 for num in range(lower, upper, + 1):
 	for i in range(2, num):
 		if(num % i) == 0:
-			print(str(num) + " is not a prime number"
+			print(str(num) + " is not a prime number")
+			break
+	else:
+		print(str(num) + " is a prime number")
+		primes.append(num)
 
+if(len(primes) > 0):
+	print("\nThe set of prime numbers in that range is :",primes,"\n")
+else:
+	print("\nThere are no prime numbers in that range\n")
