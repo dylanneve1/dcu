d9ff42ce3a114028a4daecbb9df78abfb7eb65d0
dylanneve1 <dylanneve1@gmail.com>
2018-07-17 13:31:40 +0100

    Numbers

--- test/counter.py
+++ test/counter.py
@@ -15,5 +15,6 @@ while counter >= 0:
 	time.sleep(1)
 else:
 	print("\nDone.\n")
+	# Restart script.
 	python = sys.executable
 	os.execl(python, python, *sys.argv)
---
+++ test/numbers.py
@@ -0,0 +1,16 @@
+def main(args):
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    
+    a = 20
+    b = 10
+    c = 15
+    d = 5
+    answer = 0
+    
+    answer = c * b
+    print"Value of c * b is", answer
+    
+    sys.exit(main(sys.argv))
