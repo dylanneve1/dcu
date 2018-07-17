5081e6c32f970411c926e14f82d61fc07fe6245c
dylanneve1 <dylanneve1@gmail.com>
2018-07-17 12:44:01 +0100

    counter = counter - 1 --> counter -= 1

--- test/counter.py
+++ test/counter.py
@@ -5,11 +5,11 @@ import time
 counter = int(input("Enter a number : "))
 print(" ")
 
-# Print the value of counter going down by one every second.
+# Print the value of counter going down by one every second. 
 # If counter is at 0, display "Done".
 while counter >= 0:
 	print(str(counter))
-	counter = counter - 1
+	counter -= 1
 	time.sleep(1)
 else:
 	print("\nDone.")
