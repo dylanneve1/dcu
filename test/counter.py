a5f44b656246487c0d12da246b1849ee0d1e0f75
dylanneve1 <dylanneve1@gmail.com>
2018-07-17 12:37:30 +0100

    Add comments and add spacing

--- test/counter.py
+++ test/counter.py
@@ -1,10 +1,15 @@
+# Imports. 
 import time
 
+# Get counter value.
 counter = int(input("Enter a number : "))
+print(" ")
 
+# Print the value of counter going down by one every second.
+# If counter is at 0, display "Done".
 while counter >= 0:
 	print(str(counter))
 	counter = counter - 1
 	time.sleep(1)
 else:
-	print("Done.")
+	print("\nDone.")
