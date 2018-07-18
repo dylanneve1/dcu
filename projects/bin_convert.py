61538c44beff416f9455cbc16a2c98fab906451f
dylanneve1 <dylanneve1@gmail.com>
2018-07-18 19:05:24 +0100

    yeet

--- projects/bin_convert.py
+++ projects/bin_convert.py
@@ -1,4 +1,6 @@
 import time
+import os
+import sys
 
 class binaryConverter:
 
@@ -6,20 +8,32 @@ class binaryConverter:
 
 		print("\nConversion options :\n")
 		print("1 - Number to binary.")
-		print("2 - Binary to number.\n")
+		print("2 - Binary to number.")
+		print("3 - Add binary.")
+		print("4 - Subtract binary.\n")
 		mode = int(input("Selection : "))
-		print("\n\n")
+		print("\n")
 
 		if mode is 1:
 			inputNumber = input("Enter number : ")
-			print("\n\n")
-			print(bin(inputNumber))
+			print("\n" + bin(inputNumber))
 
 		elif mode is 2:
-			inputNumber = input("Enter binary : ")
-			print("\n\n")
-			convertedValue = str(inputNumber)
-			print(convertedValue)
+			inputNumber = "0b" + str(input("Enter binary : "))
+			finalValue = int(inputNumber, 2)
+			print("\n" + str(finalValue))
+
+		elif mode is 3:
+			firstNumber = "0b" + str(input("Enter first number : "))
+			secondNumber = "0b" + str(input("Enter second number : "))
+			addedValues = (int(firstNumber, 2)) + (int(secondNumber, 2))
+			print("\n" + bin(addedValues))
+
+		elif mode is 4:
+			firstNumber = "0b" + str(input("Enter first number : "))
+			secondNumber = "0b" + str(input("Enter second number : "))
+			subtractedValues = (int(firstNumber, 2)) - (int(secondNumber, 2))
+			print(bin(subtractedValues))
 
 		else:
 			exit()
@@ -27,3 +41,7 @@ class binaryConverter:
 b = binaryConverter()
 
 b.main()
+
+# Restart script.
+python = sys.executable
+os.execl(python, python, *sys.argv)
