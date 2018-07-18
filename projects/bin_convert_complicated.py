9bc8fefb321dbfff7853b97704790bd294da0f22
dylanneve1 <dylanneve1@gmail.com>
2018-07-18 19:31:31 +0100

    Add a more complicated binary calculator skeleton

---
+++ projects/bin_convert_complicated.py
@@ -0,0 +1,28 @@
+import time
+import os
+import sys
+
+class binaryConverter:
+
+	def main(self):
+		print("\nOptions :\n")
+		print("1 - Add binary.")
+		print("2 - Subtract binary.\n")
+
+		mode = int(input("Choice : "))
+
+		if mode is 1:
+			
+
+		elif mode is 2:
+			
+
+		else:
+			print("\nINVALID OPTION")
+			exit()
+
+
+
+b = binaryConverter()
+
+b.main()
