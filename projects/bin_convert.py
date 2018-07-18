b907ae0b7ccc2886d107a3166f47c91f18ed592d
dylanneve1 <dylanneve1@gmail.com>
2018-07-18 18:31:03 +0100

    Start bin converter

---
+++ projects/bin_convert.py
@@ -0,0 +1,14 @@
+print("\nConversion options :\n")
+print("1 - number to binary.")
+print("2 - binary to number.\n")
+mode = int(input("Selection : "))
+print("\n\n")
+
+if mode is 1:
+	inputNumber = int(input("Enter number : ")
+
+elif mode is 2:
+	inputNumber = int(input("Enter binary : ")
+
+else:
+	exit()
